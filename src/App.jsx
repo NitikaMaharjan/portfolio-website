b3c685ce366ba66c3adaf7e6000324c041f0e930
Keppiekip <maharjanitika@gmail.com>
2026-01-10 09:42:37 +0545

    divide screen into two halves

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@ import './App.css'
 
 export default function App() {
   return (
-    <>
-      <h1 className="text-amber-300">App</h1>
-    </>
+    <div className="grid grid-cols-2">
+      <div className="h-screen border overflow-auto p-28 flex justify-end">
+        <h1>Nitika Maharjan</h1>
+      </div>
+      <div className="h-screen border pt-28 pl-28">
+        <p>Hello world!</p>
+      </div>
+    </div>
   )
 }
